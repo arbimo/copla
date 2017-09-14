cf25e91815ccd7d54e3bc19c6a081f4997490d18
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2017-09-14 14:37:56 +0200

    [lang] Make simplification accessible through the command line.

--- copla-lang/src/main/scala/copla/lang/ParserApp.scala
+++ copla-lang/src/main/scala/copla/lang/ParserApp.scala
@@ -2,10 +2,10 @@ package copla.lang
 
 import java.io.File
 
+import copla.lang.model.transforms
 import copla.lang.parsing.anml.{GenFailure, ParseFailure, ParseSuccess, Parser}
 
-case class Config(anmlFile: File = new File("."),
-                  quiet: Boolean = false)
+case class Config(anmlFile: File = new File("."), quiet: Boolean = false, simplify: Boolean = false)
 
 object ParserApp extends App {
 
@@ -17,6 +17,10 @@ object ParserApp extends App {
       .text("Do not print the result of parsing.")
       .action((_, c) => c.copy(quiet = true))
 
+    opt[Unit]('s', "simplify")
+      .text("Simplify the model to ANML core.")
+      .action((_, c) => c.copy(simplify = true))
+
     arg[File]("anml-file")
       .text("ANML file to parse.")
       .action((f, c) => c.copy(anmlFile = f))
@@ -26,8 +30,14 @@ object ParserApp extends App {
     case Some(conf) =>
       Parser.parse(conf.anmlFile) match {
         case ParseSuccess(model) =>
-          if (!conf.quiet)
-            println(model)
+          if (conf.simplify) {
+            val core = transforms.FullToCore.trans(model, transforms.Config())
+            if (!conf.quiet)
+              println(core.mkString("\n"))
+          } else {
+            if (!conf.quiet)
+              println(model)
+          }
         case x: GenFailure =>
           println(x.format)
           sys.exit(1)
