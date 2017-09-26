bec5b3a36e7bc4091633a688a7fcfa41943d0203
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2017-09-26 13:57:50 +0200

    [lang] Add unit tests for invariants of core models.

--- copla-lang/src/test/scala/copla/lang/model/transforms/FullToCoreTest.scala
+++ copla-lang/src/test/scala/copla/lang/model/transforms/FullToCoreTest.scala
@@ -1,9 +1,19 @@
 package copla.lang.model.transforms
 
+import copla.lang.model.core._
 import copla.lang.parsing.anml.{InputAnmlModels, ParseFailure, ParseSuccess, Parser}
 import org.scalatest.FunSuite
 
 class FullToCoreTest extends FunSuite {
+
+  def asCore(anml: String): Either[String,CoreModel] = {
+    Parser.parse(anml) match {
+      case ParseSuccess(mod) =>
+        Right(FullToCore.trans(mod))
+      case _ =>
+        Left("Could not parse anml string")
+    }
+  }
 
   for (anml <- InputAnmlModels.valid) {
     test("translation to core: " + anml) {
@@ -11,8 +21,26 @@ class FullToCoreTest extends FunSuite {
         case ParseSuccess(module) =>
           println(FullToCore.trans(module).mkString("\n"))
         case x: ParseFailure =>
-          // ignore, this should be caught by the ANML parsing tests
+        // ignore, this should be caught by the ANML parsing tests
       }
     }
+  }
+
+  for (anml <- InputAnmlModels.valid) {
+    test("Invariant on core: " + anml) {
+      Parser.parse(anml) match {
+        case ParseSuccess(fullMod) =>
+          val m = FullToCore.trans(fullMod)
+          checkInvariantsInCore(m)
+        case _ =>
+          fail("Could not parse anml string")
+      }
+    }
+  }
+
+  def checkInvariantsInCore(m: CoreModel): Unit = {
+    val declarations = m.collect { case x: Declaration[_] => x }
+    assert(declarations.distinct.size == declarations.size,
+      "\nDuplicate declarations in: \n"+declarations.mkString("\n"))
   }
 }
