823f87accb7c975327afd6ecf9e44fd7596eeacb
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2017-10-10 18:07:30 +0200

    [constraints] Additional test case.

--- copla-constraints/src/test/scala/copla/constraints/meta/DisjunctiveConstraintTest.scala
+++ copla-constraints/src/test/scala/copla/constraints/meta/DisjunctiveConstraintTest.scala
@@ -1,10 +1,16 @@
 package copla.constraints.meta
 
+import copla.constraints.meta.constraints.{Contradiction, Tautology}
+import copla.constraints.meta.domains.Domain
 import copla.constraints.meta.variables.IntVariable
 import org.scalatest.{BeforeAndAfter, FunSuite}
 import copla.constraints.meta.search.{BinarySearch, Solution}
 
 class DisjunctiveConstraintTest extends FunSuite with BeforeAndAfter {
+
+
+  slogging.LoggerConfig.factory = slogging.PrintLoggerFactory()
+  slogging.LoggerConfig.level = slogging.LogLevel.WARN
 
   implicit var csp: CSP = null
   var v1, v2, v3: IntVariable = null
@@ -12,13 +18,28 @@ class DisjunctiveConstraintTest extends FunSuite with BeforeAndAfter {
   before {
     csp = new CSP
     v1 = csp.variable("v1", Set(1,2,3))
-    v2 = csp.variable("v2", Set(2))
+    v2 = csp.variable("v2", Set(2, 3))
     v3 = csp.variable("v3", Set(1, 2, 3))
   }
 
   test("Disjunction constraint enforced by search") {
     csp.post(v2 =!= v1 || v2 =!= v3)
     csp.propagate()
+    val res = BinarySearch.search(csp)
+    res match {
+      case Solution(sol) =>
+        println(sol.report)
+        assert(sol.isSolution)
+      case _ =>
+        assert(false)
+    }
+  }
+
+  test("Same constraint posted and watched") {
+    val c = v1 =!= v2
+    val c2 = v1 === v2
+    val mc = (c || c2) && (v1 =!= v3)
+    csp.post(mc)
     val res = BinarySearch.search(csp)
     res match {
       case Solution(sol) =>
