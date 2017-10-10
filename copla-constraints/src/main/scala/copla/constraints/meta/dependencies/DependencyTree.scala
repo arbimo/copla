d247af2e84bcd65385c709c2e6e240cd6348b5c8
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2017-10-10 15:38:47 +0200

    [constraints] First implementation of dependency tree.

---
+++ copla-constraints/src/main/scala/copla/constraints/meta/dependencies/DependencyTree.scala
@@ -0,0 +1,86 @@
+package copla.constraints.meta.dependencies
+
+import copla.constraints.meta.constraints.Constraint
+import copla.constraints.meta.variables.IVar
+
+import scala.collection.mutable
+
+sealed trait RootOrNode
+case object Root extends RootOrNode
+sealed trait Node extends RootOrNode
+case class Cst(c: Constraint) extends Node
+case class Var(v: IVar) extends Node
+
+
+
+
+class DependencyTree(optBase: Option[DependencyTree] = None) {
+
+  val tree: mutable.Map[RootOrNode, mutable.Set[Node]] = optBase match {
+    case Some(base) => mutable.Map(base.tree.mapValues(_.clone).toSeq: _*)
+    case None => mutable.Map(Root -> mutable.Set())
+  }
+
+  val inverseTree: mutable.Map[Node, mutable.Set[RootOrNode]] = optBase match {
+    case Some(base) => mutable.Map(base.inverseTree.mapValues(_.clone).toSeq: _*)
+    case None => mutable.Map()
+  }
+
+  private def addEdgeUnsafe(from: RootOrNode, to: Node): Unit = {
+    tree.getOrElseUpdate(from, mutable.Set()) += to
+    inverseTree.getOrElseUpdate(to, mutable.Set()) += from
+  }
+  private def deleteEdgeUnsafe(from: RootOrNode, to: Node): Unit = {
+    tree.getOrElse(from, mutable.Set()) -= to
+    inverseTree.getOrElse(to, mutable.Set()) -= from
+  }
+
+  def numChildren(node: Node): Int = tree.get(node).map(_.size).getOrElse(0)
+  def numParents(node: Node): Int = inverseTree.get(node).map(_.size).getOrElse(0)
+
+  def children(node: RootOrNode): Set[Node] = tree.get(node).map(_.toSet).getOrElse(Set())
+  def parents(node: Node): Set[RootOrNode] = inverseTree.get(node).map(_.toSet).getOrElse(Set())
+
+  def contains(node: RootOrNode): Boolean = node match {
+    case Root => assert(tree.contains(Root)); true
+    case x: Node => inverseTree.contains(x)
+  }
+
+  def addEdge(from: RootOrNode, to: Node, children: Node => Set[Node]): Seq[Node] = {
+    require(contains(from))
+    if(contains(to)) {
+      addEdgeUnsafe(from, to)
+      Nil
+    } else {
+      addEdgeUnsafe(from, to)
+      val recursivelyAdded =
+        for(child <- children(to).toSeq ; n <- addEdge(to, child, children)) yield n
+      recursivelyAdded :+ to
+    }
+  }
+
+  def removeEdge(from: RootOrNode, to: Node): Seq[Node] = {
+    deleteEdgeUnsafe(from, to)
+    numParents(to) match {
+      case 0 =>
+        val subnodes = children(to).toSeq
+        val recursivelyRemoved =
+          for (sub <- subnodes) yield removeEdge(to, sub)
+        assert(numParents(to) == 0)
+        assert(numChildren(to) == 0)
+        inverseTree -= to
+        recursivelyRemoved.flatten :+ to
+      case _ =>
+        // node still has parents, don't touch anything.
+        Nil
+    }
+  }
+
+  override def clone(): DependencyTree = new DependencyTree(Some(this))
+
+}
+
+
+object DependencyTree {
+
+}
---
+++ copla-constraints/src/test/scala/copla/constraints/meta/dependencies/DependenciesTest.scala
@@ -0,0 +1,53 @@
+package copla.constraints.meta.dependencies
+
+import copla.constraints.meta.CSP
+import copla.constraints.meta.constraints.{Constraint, Tautology}
+import org.scalatest.FunSuite
+
+class DependenciesTest extends FunSuite {
+
+  implicit val csp = new CSP()
+  val v1 = csp.variable("v1", Set(1,2,3))
+  val v2 = csp.variable("v2", Set(1,2,3))
+  val v3 = csp.variable("v3", Set(1,2,3))
+
+
+  val dep: Node => Set[Node] = _ match {
+    case Var(v) => Set()
+    case Cst(c) =>
+      c.subconstraints.map(Cst(_)).toSet[Node] ++ c.variables.map(Var(_))
+  }
+
+  test("constraint addition") {
+    val tree = new DependencyTree()
+    val c1 = v1 === v2 || v1 === v3
+    val c2 = v3 === v2
+    val c3 = c1 && c2
+
+    implicit def constraint2Node(c: Constraint): Cst = Cst(c)
+
+
+    tree.addEdge(Root, c3, dep)
+
+    assert(tree.children(c3).contains(c1))
+    assert(tree.children(c3).contains(c2))
+    assert(tree.parents(c1).contains(c3))
+
+    tree.addEdge(Root, c2, dep)
+
+    assert(tree.parents(c2).contains(Root))
+    assert(tree.children(Root).contains(c2))
+
+    tree.removeEdge(Root, c3)
+
+    assert(tree.parents(c2).contains(Root))
+    assert(tree.children(Root).contains(c2))
+
+    assert(!tree.contains(c3))
+    assert(!tree.contains(c1))
+
+    println(tree)
+
+  }
+
+}
